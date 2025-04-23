17d792a5d3591d9f759e4cbcd18f788373f277b2
Etzio <carldrewx@gmail.com>
2025-04-23 13:29:46 +0800

    Added files

---
+++ app.js
@@ -0,0 +1,15 @@
+function gotoHome(url) {
+    window.location = url;
+}
+
+function gotoAbout(url) {
+    window.location = url;
+}
+
+function gotoMembership(url) {
+    window.location = url;
+}
+
+function gotoContact(url) {
+    window.location = url;
+}
